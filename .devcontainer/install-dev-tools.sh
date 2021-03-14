1785c1d97bf91e6e9d54223db3b91320bbf2da17
cgHome <cghome@cflat-inc.org>
2021-03-14 09:01:29 +0100

    Update project using Create Homey App

---
+++ .devcontainer/install-dev-tools.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Define homey package.json 
+npm config set init-module ./.npm-init.js
+npm init --quiet -y 1>/dev/null
+
+# Install ESLint & prettier
+npm install --save-dev --save-exact \
+  prettier
+npm install --save-dev \
+  eslint \
+  eslint-config-athom \
+  eslint-config-prettier \
+  eslint-plugin-prettier
+#npx install-peerdeps --dev \
+#  eslint-config-airbnb-base
+
+# Commiting the changes 
+git add .
+# > cannot write to ./git directory, to be fixed
+# git commit -am "Dev-Tools installed"
